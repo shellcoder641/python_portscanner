8b14f3041abb3eb72b31731934499a0b146e30fa
root <root@localhost.localdomain>
2019-08-24 00:53:58 -0400

    Fixed the issue where an exception is raised instead of just having an error indicate status report

--- python_portscanner/portscanner.py
+++ python_portscanner/portscanner.py
@@ -1,37 +1,50 @@
-#A simple port scanner that open a full 3 ways handshake TCP connection to check if the port is open or not.
-#It then tries to grab the banner for running services. Currently it "suppose" to query all the ports but...
-#ISSUE: when it queries port 22, 3 ways handshake established, then it makes another query on port 22?? and received a RST, 
-#then stop all subsequence port scan??? 
-#MISSING: doesn't know whether the port is closed or filtered by the firewall.
 import socket
 import argparse
-s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-#addr_family: AF_INET and AF_INET6: IPv4 and IPv6
-#sock_type: SOCK_STREAM, SOCK_DGRAM: TCP and UDP
-arg_parser=argparse.ArgumentParser()
-arg_parser.add_argument("host",type=str,help="Target hostname")
-arg_parser.add_argument("--port",type=int,default=80,help="Target port number")
-arg_parser.add_argument("--all,")
-args=arg_parser.parse_args()
-if args.port>0:
-	try:
-	    print("wth")
-	    s.connect((args.host,args.port))#-->s.connect(("python.org",80))
-	    info=s.recv(1024)
-	    print("[+] Port %d is open: %s"%(args.port,str(info)))
+def scanner():
+	s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+	#addr_family: AF_INET and AF_INET6: IPv4 and IPv6
+	#sock_type: SOCK_STREAM, SOCK_DGRAM: TCP and UDP
+	arg_parser=argparse.ArgumentParser()
+	arg_parser.add_argument("host",type=str,help="Target hostname")
+	group=arg_parser.add_mutually_exclusive_group(required=True)
+	group.add_argument("--port",type=int,help="Target port number")
+	group.add_argument("--all",help="Scan all ports",nargs='?', const=3)#can have 0 arguments, const 3 is for bookkeeping
+	group.add_argument("--range",type=int,nargs=2,help="A range of ports")#nargs=number of args
+	args=arg_parser.parse_args()
+	if args.port:#scan a specific port
+	    print("Scanning one specific port")
+	    res=s.connect_ex((args.host,args.port))#-->s.connect(("python.org",80))
+	    print("Trying to initiate a connection to port %d"%args.port)
+	    if res==0:
+	    	# info=s.recv(1024)#this line has been commented out because we don't know when the client will send the data
+	    	# we are trying to grab the banner but don't know whether the service actually send the banner or not
+	    	# print("[+] Port %d is open: %s"%(args.port,str(info)))
+	    	print("[+] Port %d is open."%args.port)
+	    else:
+	    	print("[-] Port %d is closed or filtered."%args.port)
 	    s.close()
-	except socket.error as e:
-		print("[-] Port %d is closed or filtered."%args.port)
-	socket.setdefaulttimeout(1)
-else:
-	for i in range(0,65536):
-		try:
-			s.connect((args.host,i))
+	elif args.all:#scan all ports
+		print("Scanning all ports")
+		for i in range(0,65536):
+			res=s.connect_ex((args.host,i))
 			print("Trying to initiate a connection to port %d"%i)
-			info=s.recv(100)
-			s.close()
-			print("[+] Port %d is open: %s"%(i,str(info)))
-		except socket.error as e:
-			print("[-] Port %d is closed or filtered."%args.port)
-			continue
-		socket.setdefaulttimeout(1)
+			if res==0:
+				print("[+] Port %d is open."%i)
+			else:
+				print("[-] Port %d is closed or filtered."%i)
+		s.close()
+	elif args.range:
+		print("Scanning a range of ports")
+		for i in range(args.range[0],args.range[1]):
+			res=s.connect_ex((args.host,i))
+			print("Trying to initiate a connection to port %d"%i)
+			if res==0:
+				print("[+] Port %d is open."%i)
+			else:
+				print("[-] Port %d is closed or filtered."%i)
+		s.close()
+
+def main():
+	scanner()
+if __name__=="__main__":
+	main()
